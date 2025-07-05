3535a253b3d872c7784ce2007d3aa7e4859aba94
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-05 17:39:53 -0600

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './main.css'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  defaultPreloadStaleTime: Infinity,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tanstackRouter({ target: 'react' }), tailwindcss(), react()],
+})
