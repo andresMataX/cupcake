524d4c64c91f4e1f34a41a46f8c0fc73a7d99f1e
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-05 18:18:57 -0600

    Add gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cupcake',
   plugins: [tanstackRouter({ target: 'react' }), tailwindcss(), react()],
 })
