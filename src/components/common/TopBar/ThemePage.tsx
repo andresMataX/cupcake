0972a612aadaff9d6f6b88ca63ba149916073f05
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-06 19:17:30 -0600

    Add theme page content

--- src/components/common/TopBar/ThemePage.tsx
+++ src/components/common/TopBar/ThemePage.tsx
@@ -1,11 +1,45 @@
-import type { FC } from 'react'
+import { useState, type FC } from 'react'
+import { DayPicker } from 'react-day-picker'
 
 interface Props {}
 
 export const ThemePage: FC<Props> = () => {
+  const [date, setDate] = useState<Date>()
+
   return (
-    <div>
-      <h1 className="text-xl font-semibold">Cupcake</h1>
+    <div className="flex flex-col gap-2">
+      <h1 className="text-xl font-semibold">Información de Cupcake</h1>
+
+      <p className="text-sm">
+        Aquí puedes cambiar la información relacionada a la aplicación Cupcake.
+      </p>
+
+      <fieldset className="fieldset">
+        <legend className="fieldset-legend pb-1">Fecha de nacimiento</legend>
+
+        <button
+          popoverTarget="rdp-popover"
+          className="input input-border w-full"
+          style={{ anchorName: '--rdp' } as React.CSSProperties}
+        >
+          {date ? date.toLocaleDateString() : 'Pick a date'}
+        </button>
+      </fieldset>
+
+      <div
+        popover="auto"
+        id="rdp-popover"
+        className="dropdown"
+        style={{ positionAnchor: '--rdp' } as React.CSSProperties}
+      >
+        <DayPicker
+          selected={date}
+          onSelect={setDate}
+          className="react-day-picker"
+          captionLayout="dropdown"
+          mode="single"
+        />
+      </div>
     </div>
   )
 }
