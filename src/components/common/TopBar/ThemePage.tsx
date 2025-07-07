0bd8686ef65a9035401c63c1b4a6dd2ed0e885b7
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-06 20:24:15 -0600

    Add form to theme page

--- src/components/common/TopBar/ThemePage.tsx
+++ src/components/common/TopBar/ThemePage.tsx
@@ -1,6 +1,7 @@
 import { useUser } from '@clerk/clerk-react'
+import { useForm } from '@tanstack/react-form'
 import { format } from 'date-fns'
-import { useState, type FC } from 'react'
+import { type FC } from 'react'
 
 import { DayPicker } from 'react-day-picker'
 import { MdOutlineCalendarMonth } from 'react-icons/md'
@@ -10,9 +11,14 @@ interface Props {}
 export const ThemePage: FC<Props> = () => {
   const { user } = useUser()
 
-  const [date, setDate] = useState<Date>(
-    user?.unsafeMetadata.cupcake?.birthday ?? new Date(),
-  )
+  const form = useForm({
+    defaultValues: {
+      date: user?.unsafeMetadata.cupcake?.birthday ?? new Date(),
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value)
+    },
+  })
 
   if (!user) return <></>
 
@@ -27,38 +33,55 @@ export const ThemePage: FC<Props> = () => {
         </p>
       </div>
 
-      <fieldset className="fieldset">
-        <legend className="fieldset-legend pb-1">Fecha de nacimiento</legend>
-
-        <button
-          popoverTarget="rdp-popover"
-          className="input input-border w-full"
-          style={{ anchorName: '--rdp' } as React.CSSProperties}
-        >
-          <MdOutlineCalendarMonth className="mr-1" />
-          {format(
-            date,
-            user.unsafeMetadata.general?.date_format || 'dd/MM/yyyy',
-          )}
-        </button>
-      </fieldset>
-
-      <div
-        popover="auto"
-        id="rdp-popover"
-        className="dropdown"
-        style={{ positionAnchor: '--rdp' } as React.CSSProperties}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          form.handleSubmit()
+        }}
       >
-        <DayPicker
-          selected={date}
-          defaultMonth={date}
-          onSelect={setDate}
-          className="react-day-picker"
-          captionLayout="dropdown"
-          mode="single"
-          required
+        <form.Field
+          name="date"
+          children={(field) => (
+            <>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend pb-1">
+                  Fecha de nacimiento
+                </legend>
+
+                <button
+                  type="button"
+                  popoverTarget="rdp-popover"
+                  className="input input-border w-full"
+                  style={{ anchorName: '--rdp' } as React.CSSProperties}
+                >
+                  <MdOutlineCalendarMonth className="mr-1" />
+                  {format(
+                    field.state.value,
+                    user.unsafeMetadata.general?.date_format || 'dd/MM/yyyy',
+                  )}
+                </button>
+              </fieldset>
+
+              <div
+                popover="auto"
+                id="rdp-popover"
+                className="dropdown"
+                style={{ positionAnchor: '--rdp' } as React.CSSProperties}
+              >
+                <DayPicker
+                  selected={field.state.value}
+                  defaultMonth={field.state.value}
+                  onSelect={field.handleChange}
+                  className="react-day-picker"
+                  captionLayout="dropdown"
+                  mode="single"
+                  required
+                />
+              </div>
+            </>
+          )}
         />
-      </div>
+      </form>
     </div>
   )
 }
