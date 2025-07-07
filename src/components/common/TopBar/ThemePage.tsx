0f0c4ec5a7eda340f2ec766cb59a513dd17df9b7
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-06 19:01:09 -0600

    Add theme page to user profile

---
+++ src/components/common/TopBar/ThemePage.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react'
+
+interface Props {}
+
+export const ThemePage: FC<Props> = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">Cupcake</h1>
+    </div>
+  )
+}
---
+++ src/components/common/TopBar/TopBar.tsx
@@ -0,0 +1,38 @@
+import { UserButton } from '@clerk/clerk-react'
+import { Link } from '@tanstack/react-router'
+import type { FC } from 'react'
+import { GiCupcake } from 'react-icons/gi'
+import { ThemePage } from './ThemePage'
+
+interface Props {}
+
+export const TopBar: FC<Props> = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link
+          to="/"
+          className="btn btn-primary"
+        >
+          <span>Cupcake</span>
+
+          <GiCupcake className="size-5" />
+        </Link>
+
+        <UserButton>
+          <UserButton.UserProfilePage
+            label="Settings"
+            url="custom"
+            labelIcon={<GiCupcake />}
+          >
+            <ThemePage />
+          </UserButton.UserProfilePage>
+
+          <UserButton.UserProfilePage label="account" />
+
+          <UserButton.UserProfilePage label="security" />
+        </UserButton>
+      </div>
+    </div>
+  )
+}
