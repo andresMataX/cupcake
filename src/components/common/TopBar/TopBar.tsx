e35074ab6424bfd88d5fa3779e6a97e9917decc1
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-06 21:50:44 -0600

    Add locale swapper to top bar

--- src/components/common/TopBar/TopBar.tsx
+++ src/components/common/TopBar/TopBar.tsx
@@ -1,12 +1,28 @@
+import { getLocale, setLocale } from '@/paraglide/runtime'
 import { UserButton } from '@clerk/clerk-react'
 import { Link } from '@tanstack/react-router'
 import type { FC } from 'react'
 import { GiCupcake } from 'react-icons/gi'
 import { ThemePage } from './ThemePage'
 
+const flag = {
+  es: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg',
+  en: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/250px-Flag_of_the_United_States_%28Pantone%29.svg.png',
+}
+
 interface Props {}
 
 export const TopBar: FC<Props> = () => {
+  const locale = getLocale()
+
+  const onSwitchLanguage = () => {
+    if (locale === 'es') {
+      setLocale('en')
+    } else {
+      setLocale('es')
+    }
+  }
+
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="container mx-auto flex items-center justify-between">
@@ -19,19 +35,32 @@ export const TopBar: FC<Props> = () => {
           <span>Cupcake</span>
         </Link>
 
-        <UserButton>
-          <UserButton.UserProfilePage
-            label="Settings"
-            url="custom"
-            labelIcon={<GiCupcake />}
+        <div className="flex items-center gap-3">
+          <button
+            className="btn btn-dash btn-primary btn-circle btn-sm"
+            onClick={onSwitchLanguage}
           >
-            <ThemePage />
-          </UserButton.UserProfilePage>
+            <div className="avatar">
+              <div className="w-5 rounded-full">
+                <img src={flag[locale]} />
+              </div>
+            </div>
+          </button>
 
-          <UserButton.UserProfilePage label="account" />
+          <UserButton>
+            <UserButton.UserProfilePage
+              label="Settings"
+              url="custom"
+              labelIcon={<GiCupcake />}
+            >
+              <ThemePage />
+            </UserButton.UserProfilePage>
 
-          <UserButton.UserProfilePage label="security" />
-        </UserButton>
+            <UserButton.UserProfilePage label="account" />
+
+            <UserButton.UserProfilePage label="security" />
+          </UserButton>
+        </div>
       </div>
     </div>
   )
