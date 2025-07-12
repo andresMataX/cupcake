69467b0c850b6704db988afaba98e329336369aa
Jesús Andrés Mata <andresmata.c21@gmail.com>
2025-07-11 19:13:26 -0600

    Add base bg color to splash screen

--- src/components/common/SplashScreen.tsx
+++ src/components/common/SplashScreen.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 export const SplashScreen: FC<Props> = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center p-2">
+    <div className="bg-base-100 flex h-screen w-screen flex-col items-center justify-center p-2">
       <div className="size-32">
         <DotLottieReact
           src="https://lottie.host/f0ed2910-b21b-487e-8003-b69e7594b6ab/EnfxQDhbOY.lottie"
